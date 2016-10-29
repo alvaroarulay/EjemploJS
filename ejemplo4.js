4aea18abbae4b622fb3fb3a1b2db742a857c42d7
alvaroarulay <alvarolp1@gmail.com>
2016-10-29 09:56:36 -0400

    + cambios

--- ejemplo4.js
+++ ejemplo4.js
@@ -1,6 +1,9 @@
-console.log("Suma de Dos numeros"+(3+7);
-console.log );
+console.log("Suma de Dos numeros"+(3+7));
 console.log("Resta de los numeros 13-7:");
 console.log(13-7);
 console.log("expresion con parentesis (8*2-4)/3:");
 console.log((8*2-4)/3);
+console.log("=>"+(13+7));
+console.log("=>"+"13"+"7");
+console.log("=>"+"13"+7);
+console.log(+"40"+"50");
---
+++ ejemplo7.js
@@ -0,0 +1,9 @@
+//typeof
+console.log("typeof 7 =>" + typeof 7)
+console.log("typeof \"hola\"=>"+typeof "hola")
+console.log("typeof true =>"+typeof true)
+console.log("typeof undefined =>"+ typeof undefined)
+console.log("typeof null =>" + typeof null)
+console.log("typeof new Date() =>"+ typeof new Date())
+console.log("typeof new Function() =>" + typeof new Function() )
+console.log(typeof (+"15"+1))
